14d6e815a2b7f4244ed113df2ad14c2d5d17ec00
Matthew Starkey <Matthew.Starkey@derivco.com>
2017-06-09 15:15:17 +0200

    Full Working Version

---
+++ TokenMaster/Migrations/201706091314078_Working.cs
@@ -0,0 +1,16 @@
+namespace TokenMaster.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Working : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- TokenMaster/Models/EventDevice.cs
+++ TokenMaster/Models/EventDevice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,13 @@ namespace TokenMaster.Models
     public class EventDevice
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
         public Guid Id { get; set; }
+
         public Guid StandId { get; set; }
         public string Name { get; set; }
+
         [ForeignKey("StandId")]
         public virtual EventStand EventStand { get; set; }
-
     }
 }
